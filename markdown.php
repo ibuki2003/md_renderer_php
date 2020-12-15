efcfc30c96ebb1e061007938b540203f35a44c90
ibuki2003 <main@fuwa.dev>
2020-12-15 22:27:15 +0900

    feat: support non-index files

--- markdown.php
+++ markdown.php
@@ -1,7 +1,12 @@
 <?php
 
 function get_file_path($uri) {
-    return __DIR__ . '/data/' . $uri . '/index.md';
+    $base = __DIR__ . '/data/' . $uri;
+    if (substr($base, -1) === '/') $base = substr($base, 0, -1);
+
+    if (file_exists($base . '.md'))
+        return $base . '.md';
+    return $base . '/index.md';
 }
 
 function get_md_title($content) {
