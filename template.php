92f574bf3f13664fcae5a06805869272efceefe2
ibuki2003 <main@fuwa.dev>
2020-12-15 22:42:30 +0900

    fix: change navbar-brand href to /

--- template.php
+++ template.php
@@ -28,7 +28,7 @@
     <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
             <div class="container-fluid">
-                <a href="/index"  class="navbar-brand">ふわわあのへや</a>
+                <a href="/"  class="navbar-brand">ふわわあのへや</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
