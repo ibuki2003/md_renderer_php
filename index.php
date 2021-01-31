990587921dd0987f46ab32f7f872cc524f7ccab9
ibuki2003 <main@fuwa.dev>
2021-01-31 20:25:55 +0900

    feat: support html

--- index.php
+++ index.php
@@ -4,10 +4,28 @@ require_once __DIR__ . '/GitHubMarkdownMath.php';
 require_once __DIR__ . '/markdown.php';
 
 $file = get_file_path($_SERVER["REQUEST_URI"]);
-$markdown = file_get_contents($file);
-$title = get_md_title($markdown);
+if ($file === null) {
+    http_response_code(404);
+    exit;
+}
 
-$renderer = new GitHubMarkdownMath();
-$content = $renderer->parse($markdown);
+$mode = $file[1];
+$file = $file[0];
+
+$src = file_get_contents($file);
+
+$title = '';
+if ($mode === 1) {
+    $title = get_md_title($src);
+
+    $renderer = new GitHubMarkdownMath();
+    $content = $renderer->parse($src);
+} else if($mode === 2) {
+    $title = get_html_title($src);
+    $content = $src;
+} else {
+    echo $src;
+    exit;
+}
 
 include __DIR__ . '/template.php';
--- markdown.php
+++ markdown.php
@@ -3,18 +3,45 @@
 function get_file_path($uri) {
     $base = __DIR__ . '/data/' . $uri;
     if (substr($base, -1) === '/') $base = substr($base, 0, -1);
+    return _find_file($base);
+}
+
+function _find_file($base) {
+    if (file_exists($base)) {
+        if (is_dir($base)) return _find_file($base . '/index');
+        return [$base, 0];
+    }
 
     if (file_exists($base . '.md'))
-        return $base . '.md';
-    return $base . '/index.md';
+        return [$base . '.md', 1];
+
+    if (file_exists($base . '.html'))
+        return [$base . '.html', 2];
+
+    return null;
 }
 
 function get_md_title($content) {
-    preg_match('/^# (.+)$/m', $content, $matches);
+    if (preg_match('/^# (.+)$/m', $content, $matches) !== 1) return null;
+    $title = $matches[1];
+    return $title;
+}
+
+function get_html_title($content) {
+    if (preg_match('/<h1>(.+?)<\/h1>/', $content, $matches) !== 1) return null;
     $title = $matches[1];
     return $title;
 }
 
 function get_page_title($uri) {
-    return get_md_title(file_get_contents(get_file_path($uri)));
+    $file = get_file_path($uri);
+
+    $title = '';
+    if ($file !== null) {
+        if ($file[1] === 1) $title = get_md_title(file_get_contents($file[0]));
+        if ($file[1] === 2) $title = get_html_title(file_get_contents($file[0]));
+        $title ??= basename($file[0]);
+    }
+    $title ??= basename($uri);
+    return $title;
 }
